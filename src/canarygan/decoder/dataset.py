65c0677825f14096cf79a83acae13b867bfaca39
nTrouvain <nathan.trouvain@inria.fr>
2024-06-03 16:06:39 +0200

    Clean: Decoder

--- src/canarygan/decoder/dataset.py
+++ src/canarygan/decoder/dataset.py
@@ -1,6 +1,10 @@
 # Author: Nathan Trouvain at 10/20/23 <nathan.trouvain<at>inria.fr>
 # Licence: MIT License
 # Copyright: Nathan Trouvain
+"""
+Utilities to manipulate and transform canary syllable data for
+decoding.
+"""
 import yaml
 
 from pathlib import Path
@@ -19,14 +23,70 @@ def as_path(path):
 
 
 def get_spec(df, feat="feat"):
+    """Concatenate spectrograms stored in a Pandas DataFrame,
+    in column "feat". """
     return np.vstack([s[np.newaxis, ...] for s in df[feat].to_numpy()])
 
 
 class DecoderDataset:
+    """
+    Utility class to load, preprocess and save datasets
+    of canary syllable sounds.
 
+    Data must be provided as WAV files in subdirectories (relative to
+    data_dir) named after their syllable class.
+
+    Parameters
+    ----------
+    data_dir : str, optional
+        Root dataset directory.
+    checkpoint_dir : str, optional
+        Directory name for dataset transformed content and parameters.
+    split : float, default to 0.1
+        Proportion of data used as testing material.
+    sampling_rate : int, default to 16000,
+        Audio sampling rate, in Hz.
+    features : ["deltas", "spec", "mel", "mfcc-classic"], default to deltas
+        Type of features to extract:
+            - deltas: MFCC and derivatives
+            - spec: spectrogam
+            - mel: mel-spectrogam
+            - mfcc-classic: MFCC and derivatives as done in Pagliarini et al. (2018).
+    norm_audio : bool, default to False
+        If True, will normalize audio values prior to preprocessing.
+    max_length : float, default to 1.0
+        Max length of audio samples, in second. Will pad or cut samples
+        to reach that value.
+    mfcc : bool, default to False
+        If True, will return MFCC. If False, will only return MFCC
+        derivatives and second order derivatives.
+    lifter : int, default to 0
+        Liftering parameters, to rescale MFCC.
+    n_mfcc : int, default to 13
+        Number of coefficients to compute.
+    padding : str, default to "interp"
+        Padding mode for derivative computation.
+    hop_length : int, default to 128
+        Spectrogram window strides, in timesteps.
+    win_length : int, default to 256
+        Spectrogram window length, in timesteps.
+    fmin : float, default to 500
+        Lower frequency boundary, in Hz.
+    fmax : float, default to 8000
+        Upper frequency boundary, in Hz.
+    n_fft : int, default to 512
+        Number of FFT bins computed.
+    n_mels : int, default to 128
+        Number of Mel bins.
+    seed: int, default to 0
+        Random state seed.
+    """
     @classmethod
     def from_checkpoint(cls, checkpoint_dir):
-
+        """
+        Load dataset from a checkpoint directory, containing
+        saved transformed data and preprocessing parameters.
+        """
         dataset = cls()
 
         ckpt_dir = Path(checkpoint_dir)
@@ -106,6 +166,9 @@ class DecoderDataset:
         return sum([len(v) for v in self.data_files.values()])
 
     def save_params(self, to_file):
+        """
+        Save dataset and preprocessing parameters to a YAML file.
+        """
         save_file = Path(to_file)
 
         heavy_data = ["train_data", "test_data"]
@@ -124,6 +187,10 @@ class DecoderDataset:
         return self
 
     def load_params(self, from_file):
+        """
+        Load a YAML file containing dataset and preprocessing
+        parameters.
+        """
         with Path(from_file).open("r") as fp:
             params = yaml.load(fp, Loader=yaml.Loader)
         for k, v in params.items():
@@ -131,6 +198,28 @@ class DecoderDataset:
         return self
 
     def get(self, data_dir=None, reload=False):
+        """
+        Load and preprocess data.
+
+        If data was already loaded and transformed, 
+        no operation will be applied (unless reload is set to True).
+        
+        This method will populate the "train_data" and "test_data"
+        attributes.
+
+        Parameters
+        ----------
+        data_dir : str, optional
+            Path to dataset directory, if not already given.
+        reload : bool, default to False
+            If True, will perform preprocessing even if already
+            performed.
+
+        Returns
+        -------
+        Dataset
+            Self.
+        """
         if self.train_data is not None and self.test_data is not None and not reload:
             return self
 
@@ -190,6 +279,10 @@ class DecoderDataset:
         return self
 
     def checkpoint(self, checkpoint_dir=None):
+        """
+        Save the preprocessed data and preprocessing parameters
+        to disk.
+        """
         if self.checkpoint_dir is None:
             if checkpoint_dir is None:
                 raise ValueError(
