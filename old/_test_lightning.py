7e492659b407aed47d87af6b8e04ded56d66d74a
nTrouvain <nathan.trouvain@inria.fr>
2024-06-04 17:37:09 +0200

    Remove old stuff

--- old/_test_lightning.py
+++
@@ -1,8 +0,0 @@
-# Author: Nathan Trouvain at 8/31/23 <nathan.trouvain<at>inria.fr>
-# Licence: MIT License
-# Copyright: Nathan Trouvain
-from ..canarygan.train_lightning import train
-
-
-if __name__ == "__main__":
-    train(epochs=20)
